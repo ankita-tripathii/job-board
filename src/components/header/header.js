2e66b89de3186da7a41bc839f76c3c07bd822e8d
ankita-tripathii <ankitatripathii011296@gmail.com>
2024-01-22 23:55:15 +0530

    create a header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import styles from "./header.module.css";
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
    
     return (
-       
-            <div className="container sticky-top">
-            <div className= "row">
-            <div className="col-12">
-            <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> Shopping Online</NavLink>
-            </div>
-            </div>
-            </div>
+            <Container>
+            <Row sticky="top">
+            <Col lg={12}>
+              <h2 className={styles.heading}>Hacker News Job Board</h2>
+            </Col>
+            </Row>
+            </Container>
        
     )
 }
